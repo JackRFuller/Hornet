f7fcd6d582980cd45e0fcde402bf32e08feb55d7
JackRFuller <jackrfuller@outlook.com>
2017-07-02 21:11:12 +0200

    Dice Functionality Added
    - Dice now returns the correct value when rolled

---
+++ Hornet/Hornet/Assets/Scripts/GameplayObjects/Dice/DiceHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceHandler : MonoBehaviour
+{
+    //Components
+    private Rigidbody diceRB;
+
+    [SerializeField]
+    private DiceSideHandler[] diceSideHandlers;
+    private int diceValue;
+    private Vector3 diceVelocity;
+    private bool isMoving;
+    private float forceIfStatic = 100f;
+    private bool isOnGround = false;
+
+    private void Start()
+    {
+        diceRB = this.GetComponent<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+        if(!IsDiceStillMoving())
+        {
+            diceValue = GetDiceValue();
+            Debug.Log(diceValue);
+        }
+    }
+
+    private bool IsDiceStillMoving()
+    {
+        if(diceRB.velocity.sqrMagnitude > 0.05f)
+        {
+            return true;
+        }
+        else
+        {
+            diceRB.velocity = Vector3.zero;
+            return false;
+        }
+    }
+
+    private int GetDiceValue()
+    {
+        for(int i = 0; i < diceSideHandlers.Length; i++)
+        {
+            if(diceSideHandlers[i].OnGround)
+            {
+                return 7 - diceSideHandlers[i].SideValue;
+            }
+        }
+
+        return 0;
+    }
+
+}
---
+++ Hornet/Hornet/Assets/Scripts/GameplayObjects/Dice/DiceSideHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSideHandler : MonoBehaviour
+{
+
+    // Use this for initialization
+    private string groundLayer = "Default";
+    [SerializeField]
+    [Range(1,6)]
+    private int sideValue;
+    public int SideValue { get { return sideValue; } }
+    private bool onGround = false;
+    public bool OnGround { get { return onGround; } }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer(groundLayer))
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer(groundLayer))
+        {
+            onGround = false;
+        }
+    }
+}
